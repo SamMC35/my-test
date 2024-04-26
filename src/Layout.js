7b34977799914fe32efc056567c8c64d9cff0149
SamMC35 <sambaditya.35@gmail.com>
2024-04-26 17:34:10 +0530

    new file:   build.zip 	modified:   package.json 	modified:   src/Layout.js

--- src/Layout.js
+++ src/Layout.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 function Layout(){
     return (
         <><div className="container">
             <h1>Welcome to Sam's Writings</h1>
             <div className="top-bar">
-                <a href="/">Home</a>
-                <a href="/articles">Articles</a>
-                <a href="/about">About This Page</a>
+                <Link to="/">Home</Link>
+                <Link to="articles">Articles</Link>
+                <Link to="about">About This Page</Link>
                 
             </div>
         <Outlet />
